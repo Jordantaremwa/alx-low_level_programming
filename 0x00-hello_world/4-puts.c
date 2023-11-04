07fd1e2b22e997c146d81b007a662c645ad96242
Taremwa Jordan <jordantalemwa3@gmail.com>
2023-11-04 15:38:34 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include	<stdio.h>
+
+/**
+ * main	-	prints	out	a	string	to	stdout
+ * Redturn:	0	if	success
+ */
+
+
+int	main(void)
+
+{
+
+puts("\"programming	is	like	building	a	multilingual	puzzle");
+return	(0);
+
+}
