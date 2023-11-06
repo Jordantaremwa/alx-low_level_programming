c647cd8def5913fa341d0629df02353127ae4226
Taremwa Jordan <jordantalemwa3@gmail.com>
2023-11-06 07:38:47 -0800

    returning length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include	"main.h"
+
+/**
+ * _strlen	-	function	that	returns	the	length	of	a	string
+ * @s:	parameter	to	be	used
+ * Return:	length	of	the	string
+ */
+
+int _strlen(char *s)
+
+{
+
+int	i;
+int	count	=	0;
+
+	for	(i	=	0;	s[i]	!=	'\0';	i++)
+		count++;
+
+	return	(count);
+
+}
