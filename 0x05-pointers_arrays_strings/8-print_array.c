7cbe79576d7388c5aa7c3503f7cfe1da26f08e49
Taremwa Jordan <jordantalemwa3@gmail.com>
2023-11-07 04:29:45 -0800

    printing number followed by comma and newline

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include	"main.h"
+
+/**
+ * print_array	-	function	name
+ * @a:	parameter	one
+ * @n:	parameter	two
+ */
+
+void print_array(int *a, int n)
+
+{
+
+int	i;
+
+for	(i	=	0;	i	<	n;	i++)
+{
+printf("%d",	a[i]);
+{
+	if	(i	<	n	-	1)
+		printf(",	");
+}
+}
+
+}
