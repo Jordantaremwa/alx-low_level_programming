cf2083c492e82df3ab5311a754dbb8d5ec884768
Taremwa Jordan <jordantalemwa3@gmail.com>
2023-11-16 12:42:48 -0800

    returns factorial of given number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include	"main.h"
+
+/**
+ * factorial	-	function	name
+ * @n:	parameter
+ * Return:	given	fuctorial
+ */
+
+int factorial(int n)
+
+{
+if	(n	<	0)
+	return	(-1);
+else	if	(n	==	0)
+	return	(1);
+else
+	return	(n	*	factorial(n	-	1));
+}
