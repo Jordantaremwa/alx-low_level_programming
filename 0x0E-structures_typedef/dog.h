0e077790e8955807f2f27949825cca5eb2b2be45
Taremwa Jordan <jordantalemwa3@gmail.com>
2023-11-28 10:14:08 -0800

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef	_DOG_H_
+#define	_DOG_H_
+
+/**
+ *struct	dog	-	a	stuct	that	creates	a	dog	info
+ * @name:	the	name	of	the	dog
+ * @age:	the	age	of	the	dog
+ * @owner:	the	owner	of	the	dog
+ *
+ * description:	first	stgruct	with	alx
+ */
+
+struct	dog
+{
+	char	*name;
+	float	age;
+	char	*owner;
+};
+#endif
