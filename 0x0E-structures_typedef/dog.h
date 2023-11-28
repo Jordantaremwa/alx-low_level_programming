38ceceda1a230b0db29c6bb8f56128bc68c8c331
Taremwa Jordan <jordantalemwa3@gmail.com>
2023-11-28 15:44:20 -0800

    4-new_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef	_DOG_H_
 #define	_DOG_H_
 
+/**
+ * struct	dog	-	a	struct	that	creates	dog	info
+ * @name:	the	name	of	the	dog
+ * @age:	the	age	of	the	dog
+ * @owner:	the	owner	of	the	dog
+ * description:	first	struct	with	alx
+ */
+
 typedef	struct	dog
 {
 	char	*name;
@@ -10,4 +18,5 @@ typedef	struct	dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
