ed364af6329e4d234478062300e402c679e6f64d
Taremwa Jordan <jordantalemwa3@gmail.com>
2023-11-29 11:44:11 -0800

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,10 +13,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if	(array	!=	NULL	&&	cmp	!=	NULL	&&	size	>	0)
 	{
 		int	i;
+
 		for	(i	=	0;	i	<	size;	i++)
 		{
 			if	(cmp(array[i])	!=	0)
-					return	(i);
+			{
+
+				return	(i);
+			}
 		}
 	}
 
