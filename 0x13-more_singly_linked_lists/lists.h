e4c677ed786ade0e2e7d5bfc9a88d2d9f054bd26
Taremwa Jordan <jordantalemwa3@gmail.com>
2023-12-11 13:21:34 -0800

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,6 @@
 #ifndef	LISTS_H
 #define	LISTS_H
 #include	<stdlib.h>
-#include	<stdio.h>
 
 /**
  * struct	listint_s	-	singly	linked	list
@@ -22,5 +21,6 @@ listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
+int pop_listint(listint_t **head);
 
 #endif
